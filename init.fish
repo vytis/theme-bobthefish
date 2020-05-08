1616c8485a2b3b61ef0778a67147d60ded972c3b
Vytis <vytis@users.noreply.github.com>
2020-05-08 19:09:01 +0200

    Sourcing custom functions in init

---
+++ init.fish
@@ -0,0 +1,7 @@
+# Support for nix home-manager fish plugin
+
+source $plugin_dir/fish_greeting.fish
+source $plugin_dir/fish_mode_prompt.fish
+source $plugin_dir/fish_prompt.fish
+source $plugin_dir/fish_right_prompt.fish
+source $plugin_dir/fish_title.fish
